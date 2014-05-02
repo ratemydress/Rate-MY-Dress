1da7949d7c63d2452dee2a3cb1fb887db592b918
Harsha <harshssd@users.noreply.github.com>
2014-05-02 07:42:14 -0400

    Hide Aplication ID and Client ID

--- RateMyDress/src/com/innovators/ratemydress/RateMyDressApplication.java
+++ RateMyDress/src/com/innovators/ratemydress/RateMyDressApplication.java
@@ -7,7 +7,7 @@ public class RateMyDressApplication extends Application{
 
 	public void onCreate() {
 		super.onCreate();
-		Parse.initialize(this, "y6m79FAh0jqAhVEroK1ssnH892kkH8Sml8I8wwAZ", "eZzxvluDFDOvQ4rOS0fHktSpQ0UnkIzrFZ0LJqWl");	
+		Parse.initialize(this, applicationId, clientKey);
 	}
 	
 }
